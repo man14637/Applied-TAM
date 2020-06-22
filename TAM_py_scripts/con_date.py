594397abc87d5ce090a9f11bb1e8b12e2d8c232c
Michael Nelson <man3708@Michaels-MacBook-Air.local>
2020-06-21 21:41:10 -0700

    conversation date made

---
+++ TAM_py_scripts/con_date.py
@@ -0,0 +1,25 @@
+#Creating a Date conversion function
+def con_date(d):
+    if d is 'A':
+        return '200'
+    elif d is 'B':
+        return '201'
+    elif d is 'C':
+        return '202'
+    elif d is 'D':
+        return '203'
+    elif d is 'E':
+        return '204'
+    elif d is 'F':
+        return '205'
+    elif d is 'G':
+        return '206'
+    elif d is 'H':
+        return '207'
+    elif d is 'I':
+        return '208'
+    elif d is 'J':
+        return '209'
+    else:
+        return ''
+# end of Con_date
